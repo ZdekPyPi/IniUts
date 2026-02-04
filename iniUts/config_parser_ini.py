ee0b1f66ef326ee91c08adb349710ea6f49310df
Alice Campos <allicefcampos@gmail.com>
2026-02-04 17:30:11 -0300

    style: formatting

--- iniUts/config_parser_ini.py
+++ iniUts/config_parser_ini.py
@@ -1,31 +1,33 @@
 import configparser as cp
 
 
-class IniCp():
+class IniCp:
 
-    def __init__(self,ini_file,encoding=None):
-       self.ini_file = ini_file
-       self.encoding = encoding
-       self.read_ini()
-    
+    def __init__(self, ini_file, encoding=None):
+        self.ini_file = ini_file
+        self.encoding = encoding
+        self.read_ini()
+
     def read_ini(self):
-        config = cp.RawConfigParser(allow_no_value=True,comment_prefixes=(";","#"),strict=False)
+        config = cp.RawConfigParser(
+            allow_no_value=True, comment_prefixes=(";", "#"), strict=False
+        )
         config.optionxform = str
         if self.encoding:
-            with open(self.ini_file, 'r', encoding=self.encoding) as f:
+            with open(self.ini_file, "r", encoding=self.encoding) as f:
                 config.read_string(f.read())
         else:
             config.read(self.ini_file)
-        
+
         self.config_parser = config
-    
-    def write(self,section,key,value):
+
+    def write(self, section, key, value):
         if not section in self.config_parser.sections():
             self.config_parser[section] = {}
         self.config_parser[section][key] = value
-        self.config_parser.write(open(self.ini_file, 'w',encoding=self.encoding))
-    
-    def read(self,section,key):
+        self.config_parser.write(open(self.ini_file, "w", encoding=self.encoding))
+
+    def read(self, section, key):
         if not section in self.config_parser.sections():
             raise Exception("Section not found!")
         if not key in self.config_parser[section]:
@@ -35,23 +37,19 @@ class IniCp():
     def getSections(self):
         return list(self.config_parser.sections())
 
-    def getKeys(self,section):
+    def getKeys(self, section):
         if not section in self.config_parser.sections():
             raise Exception("Section not found!")
 
         return list(self.config_parser[section])
-   
-    def section2Dict(self,section):
-        dc =  dict(self.config_parser[section])
 
-        return {x:(y or None) for x,y in dc.items()}
+    def section2Dict(self, section):
+        dc = dict(self.config_parser[section])
+
+        return {x: (y or None) for x, y in dc.items()}
 
     def __iter__(self):
         sections = self.getSections()
         for sect in sections:
             # Retorna uma tupla (chave, valor) para cada iteração
             yield sect, self.section2Dict(sect)
-
-
-
-
--- iniUts/envar.py
+++ iniUts/envar.py
@@ -1,13 +1,14 @@
-from typing import Optional 
+from typing import Optional
 
-class Envar():
-    def __init__(self,key:str,default:Optional[str] = None):
-        self.key  = key
+
+class Envar:
+    def __init__(self, key: str, default: Optional[str] = None):
+        self.key = key
         self.default = default
-    
+
     def get_value(self):
         if self.default != None:
-            return os.getenv(self.key,self.default)
+            return os.getenv(self.key, self.default)
         else:
             value = os.getenv(self.key)
             if not value:
